a32f1a366f046bbd0e98aeb079d967a84b909368
bigdub3298 <worldwidewes@Wesleys-MacBook-Pro.local>
2016-09-28 16:32:57 -0600

    Adds Basic Set Up

---
+++ SolarSystem C/PlanetListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlanetListTableViewController.h
+//  SolarSystem C
+//
+//  Created by Wesley Austin on 9/28/16.
+//  Copyright © 2016 Wesley Austin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlanetListTableViewController : UITableViewController
+
+@end
